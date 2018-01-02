66a0e975981006eccfadc0a645a57cceb959ffc6
astha <asthamanchanda7@gmail.com>
2018-01-02 20:35:21 +0530

    keyword similarity

---
+++ app.py
@@ -0,0 +1,4 @@
+from app_api import flask_app
+
+if __name__ == "__main__":
+    flask_app.run(port=8080, host="0.0.0.0")
---
+++ app_api.py
@@ -0,0 +1,58 @@
+import flask
+import spacy
+from flask import Flask
+nlp = spacy.load("en_core_web_lg")
+# Setup flask app
+flask_app = Flask(__name__)
+
+
+@flask_app.route('/')
+def index():
+    return 'Index Page'
+
+# Status URL for ops purpose
+
+
+@flask_app.route('/keywords/', methods=['POST'])
+def keywords():
+    payload  = flask.request.get_json(force=True)
+    keywords    = payload["keywords"]
+    title    = payload["title"]
+    Asin = payload["Asin"]
+    result = {}  # dictionary to keep output
+    key = []  # list to store keywords and relevance_score
+    result["ASIN"] = Asin
+    response, dic = get_keyword(keywords, title)
+    # result['keywords'] = get_keyword(words, title)
+    for r in response:
+        key.append({'keyword': r, 'relevance_score': dic[r]})
+
+    result["result"] = key
+    return flask.jsonify(result)
+
+
+def get_keyword(words, title):
+    l = {}
+    li = []
+    # print (type(words))
+    doc = nlp(title)
+    for t in words:
+        doc1 = nlp(t)
+        l[t.strip()] = doc1.similarity(doc)
+        # print (l[t])
+    print (l)
+
+    # Function to sort the dictionary 'l' according to relevance_score
+    def comp(val):
+        v = val.strip()
+        if v in l.keys():
+            # print (-float(l[v]))
+            return -float(l[v])
+        else:
+            return 0
+
+    for word in sorted(words, key=comp):
+        if word.strip() in l.keys():
+            li.append(word)
+    print (li)
+    return li, l
