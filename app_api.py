3ab5847bcf71d210f42f4cd4b232996c28fe664f
astha <asthamanchanda7@gmail.com>
2018-01-08 13:41:11 +0530

    improvements

--- app_api.py
+++ app_api.py
@@ -6,9 +6,9 @@ from threading import Thread
 # Setup flask app
 flask_app = Flask(__name__)
 from app import nlp
+import operator
+
 l = {}
-li = []
-sen = []
 
 
 @flask_app.route('/')
@@ -26,8 +26,9 @@ def keywords():
     title = payload["title"]
     Asin = payload["Asin"]
     sort = payload["sort"]
+    l.clear()
 
-    if sort == True:
+    if sort:
         keywords_queue = Queue()
         max_threads = 10
         print "Populating queue"
@@ -35,23 +36,22 @@ def keywords():
             keyword = keyword.strip()
             keywords_queue.put(keyword)
 
-
-
         print "Starting %d workers" % max_threads
         for i in range(max_threads):
-            worker = Thread(target=task, args=(keywords_queue, title,))
+            worker = Thread(target=task, args=(keywords_queue, title))
             worker.setDaemon(True)
+            # threads.append(worker)
             worker.start()
+            worker.join()
         keywords_queue.join()
 
         result = {}  # dictionary to keep output
         key = []  # list to store keywords and relevance_score
         result["ASIN"] = Asin
-        response, dic = get_keyword()
-        # response, dic = li, l
-        # result['keywords'] = get_keyword(words, title)
-        for r in response:
-            key.append({'keyword': r, 'relevance_score': dic[r]})
+        # response, dic = get_keyword()
+        response = get_keyword()
+        for r in response :
+            key.append({'keyword': r[0], 'relevance_score': r[1]})
 
         result["result"] = key
         return flask.jsonify(result)
@@ -61,20 +61,8 @@ def keywords():
 
 
 def get_keyword():
-
-    def comp(val):
-        v = val.strip()
-        if v in l.keys():
-            # print (-float(l[v]))
-            return -float(l[v])
-        else:
-            return 0
-
-    for w in sorted(sen, key=comp):
-        if w.strip() in l.keys():
-            li.append(w)
-    return li, l
-    # return
+    sorted_l = sorted(l.items(), key = operator.itemgetter(1), reverse=True)
+    return sorted_l
 
 
 def task(queue, title):
@@ -82,12 +70,11 @@ def task(queue, title):
     while not queue.empty():
 
         word = queue.get()
-        queue.task_done()
-
         if word not in l.keys():
             doc1 = nlp(word)
             l[word] = doc1.similarity(doc)
-            sen.append(word)
+            # sen.append(word)
         else:
             print ("exists")
+        queue.task_done()
 
