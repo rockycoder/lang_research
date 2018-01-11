f05c27e22a8ace9dd05967ab148414e72d7c8642
astha <asthamanchanda7@gmail.com>
2018-01-11 22:07:09 +0530

    improvements

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import spacy
 nlp = spacy.load("en_core_web_lg")
 
 if __name__ == "__main__":
-    flask_app.run(port=8080, host="0.0.0.0")
+    flask_app.run(port=8080, host="0.0.0.0", threaded=True)
