bf9d7eedf80b110f5921144513c00bfd1ab7b23f
Gaurav Manchanda <gm.gbpec@gmail.com>
2018-01-13 13:34:15 +0530

    testing go style channnel

--- fast_api.py
+++ fast_api.py
@@ -1,13 +1,10 @@
 import concurrent.futures
 import flask
+import goless
 from flask import Flask
 flask_app = Flask(__name__)
 from app import nlp
 import operator
-
-l = {}
-response = {}
-comp_str = ""
 @flask_app.route('/')
 def index():
     return 'Index Page'
@@ -15,46 +12,65 @@ def index():
 
 # Status URL for ops purpose
 
+#
+# @flask_app.route('/keywords/', methods=['POST'])
+# def keywords():
+#     l = {}
+#     comp_str = ""
+#     response = {}
+#
+#     payload = flask.request.get_json(force=True)
+#     keywords = payload["keywords"]
+#     title = payload["title"]
+#     title = nlp(title)
+#     Asin = payload["Asin"]
+#     sort = payload["sort"]
+#
+#     if sort:
+#         comp_str = title
+#         with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+#
+#             for (word, score) in executor.map(get_score, keywords):
+#                 response[word] = score
+#
+#         result = {}  # dictionary to keep output
+#         key = []  # list to store keywords and relevance_score
+#         result["ASIN"] = Asin
+#         sorted_l = sorted(response.items(), key=operator.itemgetter(1), reverse=True)
+#         for r in sorted_l:
+#             key.append({'keyword': r[0], 'relevance_score': r[1]})
+#         result["result"] = key
+#         return flask.jsonify(result)
+#
+#     else :
+#         result = {"keywords": keywords}
+#         return flask.jsonify(result)
 
-@flask_app.route('/keywords/', methods=['POST'])
+
+@flask_app.route('/keywordsV2/', methods=['POST'])
 def keywords():
+    l = {}
     payload = flask.request.get_json(force=True)
     keywords = payload["keywords"]
     title = payload["title"]
     title = nlp(title)
     Asin = payload["Asin"]
     sort = payload["sort"]
-    l.clear()
-    response.clear()
-    global comp_str
-    comp_str = ""
-    if sort:
-        comp_str = title
-        with concurrent.futures.ProcessPoolExecutor(max_workers= 4) as executor:
 
-            for (word, score) in executor.map(get_score, keywords):
-                response[word] = score
+    chn = goless.chan()
 
-        result = {}  # dictionary to keep output
-        key = []  # list to store keywords and relevance_score
-        result["ASIN"] = Asin
-        sorted_l = sorted(response.items(), key=operator.itemgetter(1), reverse=True)
-        for r in sorted_l:
-            key.append({'keyword': r[0], 'relevance_score': r[1]})
-        result["result"] = key
-        return flask.jsonify(result)
+    for k in keywords:
+        goless.go(get_score, k, title,chn)
 
-    else :
-        result = {"keywords": keywords}
-        return flask.jsonify(result)
+    for k1 in keywords:
+        l[k1] = chn.recv()
+
+    return flask.jsonify(l)
 
 
-
-
-def get_score(keyword,):
+def get_score(keyword,comp_str,chn):
     doc1 = nlp(keyword)
-    l[keyword] = doc1.similarity(comp_str)
-    return keyword, l[keyword]
+    chn.send(doc1.similarity(comp_str))
 
 
 
