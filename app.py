b8f0d2d9e430a08e3db6acf8029dd8181fbd6725
Gaurav Manchanda <gm.gbpec@gmail.com>
2018-01-13 15:52:14 +0530

    testing go style channnel+2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fast_api import flask_app
-import spacy
-nlp = spacy.load("en_core_web_lg")
+import en_core_web_lg
+nlp = en_core_web_lg.load()
 
 if __name__ == "__main__":
     flask_app.run(port=8080, host="0.0.0.0", threaded=True)
