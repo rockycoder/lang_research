a814d73a2db62bc9f0d4cdfb2bdc1d32b71b1143
Gaurav Manchanda <gm.gbpec@gmail.com>
2018-02-02 13:43:38 +0530

    test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fast_api import flask_app
 import spacy
-nlp = spacy.load("en_core_web_lg")
+nlp = spacy.load("en_vectors_web_lg")
 
 if __name__ == "__main__":
     flask_app.run(port=8080, host="0.0.0.0", threaded=True)
